e6a471bd8745ee6848f60aa169ddd1c383155605
jlinaresmedalla <jlinaresmedalla@gmail.com>
2024-08-16 23:43:24 -0500

    first commit

---
+++ src/main/java/com/hotelmanagement/bookingserviceplatformbackend/BookingServicePlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hotelmanagement.bookingserviceplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServicePlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingServicePlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hotelmanagement/bookingserviceplatformbackend/BookingServicePlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelmanagement.bookingserviceplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServicePlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
